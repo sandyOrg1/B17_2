b29fc7db16fd2176afc5f634cd9d27f11cafca89
sandeepsitm91 <sandeepsitm91@gmail.com>
2025-07-08 14:24:53 +0530

    Feature/sandy2 (#4)
    * user2
    
    * user2

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
-resource "azurerm_resource_group" "rg1" {
-    name = "rg1"
+resource "azurerm_resource_group" "rg2" {
+    name = "rg2"
+    location = "West Us2"
+}
+resource "azurerm_resource_group" "rg4" {
+    name = "rg4"
     location = "West Us2"
 }
 resource "azurerm_resource_group" "rg3" {
