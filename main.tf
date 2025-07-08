119e375fb514ad37c5d597e6c0b2939b59b59fd1
sandeepsitm91 <sandeepsitm91@gmail.com>
2025-07-08 14:11:43 +0530

    Feature/sandy1 (#3)
    * user1-1
    
    * rg3

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+resource "azurerm_resource_group" "rg1" {
+    name = "rg1"
+    location = "West Us2"
+}
+resource "azurerm_resource_group" "rg3" {
+    name = "rg3"
+    location = "West Us3"
+}
 resource "azurerm_resource_group" "rg2" {
     name = "rg2"
     location = "West Us2"
